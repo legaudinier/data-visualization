726172531dd8e81bd0c54521a8b7d24ad1975124
Lindsay Gaudinier <lindsay@Lindsays-MacBook-Air.local>
2026-07-16 21:11:24 -0700

    More fixing file name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dataVisualization/',
+  base: '/data-visualization/',
   plugins: [
     react({
       babel: {
