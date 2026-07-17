cd78fef6337fb9f9bc161d83b54ed5e3930bae82
Lindsay Gaudinier <lindsay@Lindsays-MacBook-Air.local>
2026-07-16 21:27:37 -0700

    Vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/data-visualization/',
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ]
+  plugins: [react()],
+  base: '/data-visualization/', 
 })
 
